6e8597992a92327b027198dde44e1fb13634531b
Arsham Shirvani <arshamshirvani@gmail.com>
2022-02-05 18:55:20 +0000

    feat(flags): print mode
    Close #23

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/arsham/gitrelease/commit"
@@ -10,10 +11,11 @@ import (
 )
 
 var (
-	tag     string
-	rootCmd = &cobra.Command{
+	tag       string
+	printMode bool
+	rootCmd   = &cobra.Command{
 		Use:   "gitrelease",
-		Short: "Print the release information of a tag",
+		Short: "Release commit information of a tag to github",
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			token := os.Getenv("GITHUB_TOKEN")
 			if token == "" {
@@ -43,6 +45,11 @@ var (
 				}
 			}
 
+			if printMode {
+				_, err := fmt.Println(desc)
+				return err
+			}
+
 			return g.Release(token, user, repo, tag, desc)
 		},
 	}
@@ -55,4 +62,5 @@ func main() {
 func init() {
 	cobra.OnInitialize(viper.AutomaticEnv)
 	rootCmd.PersistentFlags().StringVarP(&tag, "tag", "t", "@", "tag to produce the logs for. Leave empty for current tag.")
+	rootCmd.PersistentFlags().BoolVarP(&printMode, "print", "p", false, "only print, do not release!")
 }
